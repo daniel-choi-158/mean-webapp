f8fc6678c45b80867a6feb8b8c69e717504fcbec
Daniel Choi <daniel.js.choi@gmail.com>
2019-09-29 16:04:52 +1000

    created initial transactions for JSON testing

--- backend-node/app.js
+++ backend-node/app.js
@@ -2,12 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    next();
-});
+app.use('/api/transactions', (req, res, next) => {
+    const transactions =
+        [
+            {
+                id: "drkljh4435jkh",
+                actionType: "BUY",
+                shares: "ASX:PLS",
+                quantity: "1000",
+                price: "0.3",
+                date: "01/09/2019",
+                commissions: "14.95",
+                description: " trans 1"
+            },
+            {
+                id: "gfkljh3578",
+                actionType: "BUY",
+                shares: "ASX:BHP",
+                quantity: "1000",
+                price: "23",
+                date: "01/09/2019",
+                commissions: "14.95",
+                description: " trans 2"
+            }         
+        ];
 
-app.use((req, res, next) => {
-    res.send("hello from Express!");
+    res.status(200).json({
+        message: "Successfully retrieved transactions.",
+        trasactions: transactions
+    });
 });
 
 module.exports = app;
--- frontend-angular/src/app/transactions/transaction.model.ts
+++ frontend-angular/src/app/transactions/transaction.model.ts
@@ -1,6 +1,7 @@
 export interface Transaction {
   actionType: string;
   shares: string;
+  quantity: string;
   price: string;
   date: string;
   commissions: string;
